dedc09f3578c5ca8545ae3bccef9401501cc10ed
Jesse Aldridge <jessealdridge@gmail.com>
2019-08-06 17:05:26 -0700

    delete old

--- slice_image.py
+++
@@ -1,8 +0,0 @@
-image = Image.open(image_path)
-if not os.path.exists('pieces'):
-  os.mkdir('pieces')
-width, height = image.size
-for x in range(0, width, 100):
-  for y in range(0, height, 100):
-    piece = image.crop((x, y, x + 100, y + 100))
-    piece.save('pieces/{x}x{y}.png'.format(**locals()))
